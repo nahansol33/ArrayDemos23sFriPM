ad71b54a6d877892b4e0a64f4e98e816d11b6870
Hansol Na <nahans.sheridancollege.ca>
2023-05-26 13:24:50 -0400

    This was just to practice commiting

---
+++ src/arraysdemo/PartTimeStudent.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package arraysdemo;
+
+/**
+ *
+ * @author Hansol
+ */
+public class PartTimeStudent extends Student{
+    
+    PartTimeStudent(String name, String SID){
+        super(SID, name);
+    }
+}
